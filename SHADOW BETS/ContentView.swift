30f406b586453db0b6360a456efad087f66bfec3
keshew <artyom.korotckov2017@yandex.ru>
2025-12-26 07:02:25 +0500

    Initial Commit

---
+++ SHADOW BETS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SHADOW BETS
+//
+//  Created by Артём Коротков on 26.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SHADOW BETS/SHADOW_BETSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHADOW_BETSApp.swift
+//  SHADOW BETS
+//
+//  Created by Артём Коротков on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SHADOW_BETSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
